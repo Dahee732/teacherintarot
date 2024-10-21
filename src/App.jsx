6bf9e16a2cde94bf4aee7085a9d3ceda6c069ce3
ParkJunYoung <rkt1313@naver.com>
2024-10-22 00:46:28 +0900

    노드모듈 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import Process from './components/Process';
+// import Process from './components/Process';
+import Main from './pages/Main';
+import {createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
+    
+            
     <div className="App">
-      <Process  />
+      <GlobalStyle />
+      <Main />
     </div>
+
+
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+    --primary-color: #3498db;
+    --basic-white : #fff;
+    --basic-yellow:  #FEEB81;
+  }
+  *{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  body{
+    width:100vw;
+    height:100vh;
+    overflow: hidden;
+    background: linear-gradient(174.48deg, #2B6454 47.7%, #165342 67.69%);
+  }
+
+
+`
+
+
 
 export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import scrollIcon from '../assets/images/scroll_down.svg'
+const Main = ()=> {
+
+
+    return (
+        <MainWrap>
+                <SubTitle>타로카드로 알아보는</SubTitle>
+                <MainTitle>교사 유형 테스트</MainTitle>
+
+                <ScrollIcon>
+                    <img src={scrollIcon} alt="#" />
+                    scroll
+                </ScrollIcon>
+        </MainWrap>
+    )
+    
+}
+
+const MainWrap = styled.div`
+    max-width:480px;
+    width:100%;
+    height:100%;
+    /* background-color: #fff; */
+    margin:auto;
+    color: var(--basic-white);
+    text-align: center;;
+`
+
+
+const SubTitle = styled.p`
+    color:var(--basic-yellow);
+    margin-top:100px;
+`
+const MainTitle = styled.h1`
+    color:var(--basic-white);
+`
+const ScrollIcon = styled.div`
+    position:absolute;
+    bottom:5%;
+    text-align: center;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction:column;
+    left:50%;
+    transform: translateX(-50%);
+    img{
+        display:block;
+    }
+`
+export default Main;
